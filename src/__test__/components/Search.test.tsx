a8c30dd5d2c2c72e14b3c1e9c2a06d43fb3dbe15
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-01-01 01:38:20 +0600

    feat: add blur data urls for home page images

--- src/__test__/components/Search.test.tsx
+++ src/__test__/components/Search.test.tsx
@@ -1,12 +1,11 @@
-import Search from "@/components/shared-components/Navbar/Search"
-import { render, screen } from "@testing-library/react"
+import Search from "@/components/shared-components/Navbar/Search";
+import { render, screen } from "@testing-library/react";
 
-describe('<Search/>', () => {
-   test('should Search component render Properly with a search field and placeholder text should be Search', () => {
-    render(<Search/>)
-    const searchField = screen.getByPlaceholderText('Search');
+describe("<Search/>", () => {
+  test("should Search component render Properly with a search field and placeholder text should be Search", () => {
+    render(<Search />);
+    const searchField = screen.getByPlaceholderText("Search products...");
 
     expect(searchField).toBeInTheDocument();
-   })
-
-})
+  });
+});
--- src/components/shared-components/Navbar/Search.tsx
+++ src/components/shared-components/Navbar/Search.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import styles from '@/scss/shared-component-styles/Navbar/Search.module.scss';
 
 const Search = () => {
   return (
     <form className={styles.search_form} data-search-input>
-    <input type="search" placeholder="Search" />
+    <input type="search" placeholder="Search products..." />
   </form>
   )
 }
